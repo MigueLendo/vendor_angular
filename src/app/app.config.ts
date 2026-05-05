076f83f2b7b624f8a195b9e995f4d5d54dc9e898
Miguel Henrique <miguelsaccoo@gmail.com>
2026-05-05 11:54:20 -0300

    fix routing hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation} from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { HttpInterceptor } from './shared/interceptor/http-auth-interceptor';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withFetch(), withInterceptors([HttpInterceptor])),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes, withHashLocation())
 
   ]
 };
